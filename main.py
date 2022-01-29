ce515c31dc42558f76024889c4fe21f38139caab
Artem Egorov <soldierwaat@gmail.com>
2022-01-29 20:03:51 +0200

    some fixes

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import script
 def welcome_func():
     print('Hello  my name is Artyom Egorov', end='\n')
     print("I, don't wanna conquer the world like Pinky and Brain", end='\n')
-    print('I belive i can change the world by creating something special', end='\n')
+    print('I believe i can change the world by creating something special', end='\n')
     print('You can check my linkedin account: ' + " https://www.linkedin.com/in/artem-egorov-4804a5193/", end='\n')
 
 
 
 if __name__ == '__main__':
     welcome_func()
-    a = input()
-    script.find_string(a)
+    # a = input()
+    script.find_string("likely")
--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 def find_string(str):
+    print("Checking if the string " + str +" exists in file" + '"devops_try.txt”',sep=" ")
     file = open('/Users/artyomegorov/Desktop/devops_try.txt', 'r')
     text = file.read()
     if str in text:
-        print('String ' + str + ' exists in file ' + '"devops_try.txt"')
+        print('String ' + str + ' exists in file ' + '"devops_try.txt"', sep="_")
     else:
         print('String ' + str + " does not exist in file " + '"devops_try.txt"')
 
